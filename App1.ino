d029cab12f077afc66021a6d3094cce0ed3b0eaf
DAN™ <dranger003@gmail.com>
2015-05-03 12:43:30 -0400

    Updates.

--- App1.ino
+++ App1.ino
@@ -1,16 +1,32 @@
-// App1.ino
+// hamster-crazy.ino
 //
 
 #include "stm32_it.h"
 
+#define IR_FREQ     38000
+
 // Pulse counts
 const uint8_t pcs[][2] = {
-    { 95, 72 }, // Init
-    { 16, 44 },	// 0
-    { 16, 16 }	// 1
+    { 94, 72 }, // Init
+    { 16, 16 }, // 0
+    { 16, 44 }  // 1
 };
 
-// Command sequences, first code = sequence length
+// Command sequences
+// First index = length
+const uint8_t vol_up[] = {
+    50,
+    0, 2, 2, 1, 1, 1, 1, 2, 1, 2, 2, 1, 1, 2, 1, 2, 1, 2, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 2, 2, 1, 1, 1, 1, 1, 1
+};
+
+const uint8_t vol_down[] = {
+    50,
+    0, 2, 2, 1, 1, 1, 1, 2, 1, 2, 2, 1, 1, 2, 1, 2, 1, 2, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 1, 1, 2, 2, 2, 1, 1, 1, 1, 1, 1
+};
+
+// Test sequences
 const uint8_t sq0[] = { 8, 0, 1, 1, 1, 2, 1, 2, 2 };
 const uint8_t sq1[] = { 7, 0, 2, 2, 1, 1, 2, 2 };
 
@@ -21,25 +37,23 @@ const uint8_t *sq = sq0;
 volatile uint8_t l = 0;
 volatile uint8_t r = 1;
 
-// Mark/space, pulse count and sequence index
+// Mark/space, pulse count, sequence index
 volatile uint8_t m = 1;
 volatile uint32_t p = 0;
 volatile uint8_t s = 1;
 
-// Enable output
+// Enable PWM
 void mark() {
     pinMode(RX, AF_OUTPUT_PUSHPULL);
 }
 
-// Disable output
+// Disable PWM
 void space() {
     pinMode(RX, OUTPUT);
 }
 
-// Interrupt service routine
-// A0 in PWM 38kHz connected to A1 w/interrupt on rising
 void ISR_A1() {
-	// Running mode changed?
+    // Run mode change
     if (r != l) {
         if (r)
             mark();
@@ -53,8 +67,11 @@ void ISR_A1() {
 
     if (r) {
         if (p >= pcs[sq[s]][!m]) {
+            // m = mark, !m = space
+            // !m = mark index, m = space index
             if (!m) {
-                if (++s >= sq[0]) {
+                if (++s >= sq[0] + 1) {
+                    // Sequence complete, reset
                     s = 1;
                     r = !r;
                 }
@@ -76,7 +93,7 @@ void ISR_A1() {
 }
 
 void setup() {
-	// Debug stuff
+    // Debug stuff
     Serial.begin(115200);
     while (!Serial.available()) {
         Serial.print(".");
@@ -87,25 +104,30 @@ void setup() {
 
     attachInterrupt(A1, ISR_A1, RISING);
 
-    tone(A0, 38000);
-    tone(RX, 38000);
+    tone(A0, IR_FREQ);
+    tone(RX, IR_FREQ);
 
-	// Start with output disabled
+    // Start not running
     r = 0;
 }
 
 void loop() {
     if (Serial.available()) {
         uint8_t k = Serial.read();
-
-		// Command 1
         if (k == '1') {
             sq = sq0;
             r = !r;
         }
-		// Command 2
         else if (k == '2') {
             sq = sq1;
+            r = !r;
+        }
+        else if (k == 'u') {
+            sq = vol_up;
+            r = !r;
+        }
+        else if (k == 'd') {
+            sq = vol_down;
             r = !r;
         }
     }
